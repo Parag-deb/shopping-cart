d427c158e0e37112f127bd216e791e686edef1b5
Parag-deb <paragdeb.pdeb@gmail.com>
2024-08-10 16:31:49 +0600

    complete with functions and checkout button

--- app.js
+++ app.js
@@ -1,45 +1,85 @@
+// for mobile elements
+
 const phnMinus = document.getElementById("phnMinus");
 phnMinus.addEventListener("click" , function(){
-    const phnText = document.getElementById("phnText").value;
-    const phnTextNumber = parseFloat(phnText);
-    document.getElementById("phnText").value = phnTextNumber -1 ;
-    const subTotal =parseFloat(document.getElementById("subTotal").innerText) ;
-    document.getElementById("subTotal").innerText = subTotal - 1219;
+    // const phnText = document.getElementById("phnText").value;
+    // const phnTextNumber = parseFloat(phnText);
+    // document.getElementById("phnText").value = phnTextNumber -1 ;
+    // const subTotal =parseFloat(document.getElementById("subTotal").innerText) ;
+    // document.getElementById("subTotal").innerText = subTotal - 1219;
 
-    const total = parseFloat(document.getElementById("total").innerText);
-    document.getElementById("total").innerText = total - 1219;
+    // const total = parseFloat(document.getElementById("total").innerText);
+    // document.getElementById("total").innerText = total - 1219;
+
+    minus("phnText" , 1219);
 })
+
 phnPlus.addEventListener("click" , function(){
-    const phnText = parseInt(document.getElementById("phnText").value) ;
-    document.getElementById("phnText").value = phnText + 1 ;
-    const subTotal =parseFloat(document.getElementById("subTotal").innerText) ;
-    document.getElementById("subTotal").innerText = subTotal + 1219;
+    // const phnText = parseInt(document.getElementById("phnText").value) ;
+    // document.getElementById("phnText").value = phnText + 1 ;
+    // const subTotal =parseFloat(document.getElementById("subTotal").innerText) ;
+    // document.getElementById("subTotal").innerText = subTotal + 1219;
 
-    const total = parseFloat(document.getElementById("total").innerText);
-    document.getElementById("total").innerText = total + 1219;
+    // const total = parseFloat(document.getElementById("total").innerText);
+    // document.getElementById("total").innerText = total + 1219;
+
+    adding("phnText" , 1219);
 })
+
+// for cover element
 
 covMinus = document.getElementById("covMinus");
 covMinus.addEventListener("click" , function(){
-    const covText = parseInt(document.getElementById("covText").value) ;
-    document.getElementById("covText").value = covText - 1 ;
-    const subTotal =parseFloat(document.getElementById("subTotal").innerText) ;
-    document.getElementById("subTotal").innerText = subTotal - 59;
-
-    const total = parseFloat(document.getElementById("total").innerText);
-    document.getElementById("total").innerText = total - 59;
+    minus("covText" , 59)
 })
 
 covPlus.addEventListener("click" , function(){
-    const covText = parseInt(document.getElementById("covText").value) ;
-    document.getElementById("covText").value = covText + 1 ;
-    const subTotal =parseFloat(document.getElementById("subTotal").innerText) ;
-    document.getElementById("subTotal").innerText = subTotal + 59;
-
-    const total = parseFloat(document.getElementById("total").innerText);
-    document.getElementById("total").innerText = total + 59;
+    adding("covText" , 59);
 })
 
-function adding(id , number){
-    
+// for check-out button
+
+const checkOut= document.getElementById("checkOut");
+ checkOut.addEventListener("click", function(){
+    const checkOutDisplay = document.getElementById("checkOutDisplay");
+    checkOutDisplay.style.display = "block";
+ })
+
+// functions
+
+function calculateTotalSubtotal( amount ){
+    const subTotal =parseFloat(document.getElementById("subTotal").innerText) ;
+    document.getElementById("subTotal").innerText = subTotal + amount;
+
+    const total = parseFloat(document.getElementById("total").innerText);
+    document.getElementById("total").innerText = total + amount;
 }
+
+
+function adding(id , amount){
+    const textValue = parseInt(document.getElementById(id).value);
+    document.getElementById(id).value = textValue + 1 ;
+
+    // const subTotal =parseFloat(document.getElementById("subTotal").innerText) ;
+    // document.getElementById("subTotal").innerText = subTotal + amount;
+
+    // const total = parseFloat(document.getElementById("total").innerText);
+    // document.getElementById("total").innerText = total + amount;
+    
+    //these  4 line codes are in this function
+    calculateTotalSubtotal(amount);
+}
+
+function minus(id , amount){
+    const textValue = parseInt(document.getElementById(id).value);
+    document.getElementById(id).value = textValue - 1 ;
+    // const subTotal =parseFloat(document.getElementById("subTotal").innerText) ;
+    // document.getElementById("subTotal").innerText = subTotal - amount;
+
+    // const total = parseFloat(document.getElementById("total").innerText);
+    // document.getElementById("total").innerText = total - amount;
+
+     //these  4 line codes are in this function
+    calculateTotalSubtotal(-1*amount);
+}
+
